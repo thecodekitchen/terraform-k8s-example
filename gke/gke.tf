ab3e455dc4aee3d017b20c39611305ef8775a9f1
The Code Kitchen <andy@thecodekitchen.xyz>
2023-06-18 15:29:23 -0500

    Update gke.tf

--- gke/gke.tf
+++ gke/gke.tf
@@ -12,7 +12,7 @@ variable "gke_password" {
 }
 
 variable "gke_num_nodes" {
-  default     = 2
+  default     = 1
   description = "number of gke nodes"
 }
 
@@ -49,7 +49,7 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "e2-small"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
